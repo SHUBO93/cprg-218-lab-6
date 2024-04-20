2baed54cb4a30fa2ad23876e27ac37170c5be8e9
SHUBO93 <shubozhang93@gmail.com>
2024-04-20 15:14:25 -0600

    add

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     // 获取按钮元素
-    var homeBtn = document.getElementById("homeBtn");
+    var aboutmeBtn = document.getElementById("aboutmeBtn");
     var galleryBtn = document.getElementById("galleryBtn");
+    var indexBtn = document.getElementById("indexBtn");
 
     // 添加点击事件处理程序
-    homeBtn.addEventListener("click", function () {
+    aboutmeBtn.addEventListener("click", function () {
         window.location.href = "aboutMe.html"; // 跳转到 Home 页面
     });
 
     galleryBtn.addEventListener("click", function () {
         window.location.href = "gallery.html"; // 跳转到 Gallery 页面
+    });
+
+    indexBtn.addEventListener("click", function () {
+        window.location.href = "index.html"; // 跳转到 Home 页面
     });
 });
